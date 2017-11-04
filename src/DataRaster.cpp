a4d3b65ef92b3d700b16766b62bf45dc3714bff4
lukashertrich <lukashertrich@gmail.com>
2017-11-04 09:23:56 -1000

    ...

--- src/DataRaster.cpp
+++ src/DataRaster.cpp
@@ -1,8 +1,8 @@
 #include "DataRaster.h"
 
-anita::DataRaster::getSizeOfFile(const std::string &filePath){
+std::streamsize anita::DataRaster::getSizeOfFile(const std::string &filePath){
     std::ifstream dataFile(filePath, std::ifstream::in | std::ifstream::binary);
-    if(!datFile.is_open()){
+    if(!dataFile.is_open()){
         return 0;
     }
     dataFile.ignore(std::numeric_limits<std::streamsize>::max());
@@ -11,7 +11,7 @@ anita::DataRaster::getSizeOfFile(const std::string &filePath){
 }
 
 anita::DataRaster::DataRaster(const std::string &filePath){    
-    std::streamsize dataCount = getSizeOfFile(filePath);
+    std::streamsize dataCount = anita::DataRaster::getSizeOfFile(filePath);
     values = new T[dataCount];
     std::ifstream dataFile(filePath, std::ios::binary);
     dataFile.read(reinterpret_cast<char*>(values), dataCount*sizeof(T));
